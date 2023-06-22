24b7a6915cb48aad9043288b0280636ea9d510be
Sean O'Connor <so@seano.dev>
2023-06-21 18:16:26 -0600

    Update Guests component

--- src/components/Guests.tsx
+++ src/components/Guests.tsx
@@ -10,19 +10,16 @@ export default function Guests() {
   if (!data || error) return null
 
   return (
-    <div>
-      <h2 className="mb-2 text-2xl font-semibold">💁 Previous Guests</h2>
-      <div className="flex flex-col gap-4">
-        {data.guests.map((guest, index) => (
-          <div key={index} className="rounded border p-2">
-            <span className="text-sm">
-              Signed by{' '}
-              <span className="font-semibold text-blue-500">{guest.name}</span>{' '}
-              on <span className="font-semibold">{formatDate(guest.date)}</span>
-            </span>
-          </div>
-        ))}
-      </div>
+    <div className="flex flex-col gap-4">
+      {data.guests.map((guest, index) => (
+        <div key={index} className="rounded border p-2">
+          <span className="text-sm">
+            Signed by{' '}
+            <span className="font-semibold text-blue-500">{guest.name}</span> on{' '}
+            <span className="font-semibold">{formatDate(guest.date)}</span>
+          </span>
+        </div>
+      ))}
     </div>
   )
 }
