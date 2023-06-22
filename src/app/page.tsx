ea5d933d01e638777fdede9d1175656b17504136
Sean O'Connor <so@seano.dev>
2023-06-21 18:18:30 -0600

    Fix merge conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Guests from '@/components/Guests'
-<<<<<<< HEAD
+
 import SignBook from '@/components/SignBook'
 
 export default function Home() {
@@ -7,12 +7,6 @@ export default function Home() {
     <main className="container mx-auto flex flex-col gap-4 p-4">
       <SignBook />
       <hr />
-=======
-
-export default function Home() {
-  return (
-    <main className="container mx-auto p-4">
->>>>>>> 53aa4f7990e069f364fb50bf8f84972d4d8f54e1
       <Guests />
     </main>
   )
--- src/components/Guests.tsx
+++ src/components/Guests.tsx
@@ -10,11 +10,7 @@ export default function Guests() {
   if (!data || error) return null
 
   return (
-<<<<<<< HEAD
-    <div className="flex flex-col gap-4">
-=======
     <div className="flex flex-col gap-2">
->>>>>>> 53aa4f7990e069f364fb50bf8f84972d4d8f54e1
       {data.guests.map((guest, index) => (
         <div key={index} className="rounded border p-2">
           <span className="text-sm">
